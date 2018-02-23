6cb3b6dc1ab94607f5419c1b0931e9de020ba7bd
Nicklas Laine Overgaard <nicklas@skarp.dk>
2018-02-23 15:25:48 +0100

    Update deps and make code hapi v17 compliant
    - Add hapi v17 as peer dependency
    - add engines requirement for node 8 LTS
    - upgrade hoek
    - update code to use new hapi api

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-var Hoek = require('hoek');
+const Hoek = require('hoek');
 
-var internals = {};
+const internals = {};
 
-exports.register = function (plugin, options, next) {
-
+const register = async (plugin, options) => {
     plugin.auth.scheme('fake', internals.implementation);
-    next();
 };
-
-exports.register.attributes = {
-    pkg: require('./package.json')
-};
-
 internals.implementation = function (server, options) {
 
     Hoek.assert(options, 'Missing fake auth strategy options');
     Hoek.assert(typeof options.dataFunc === 'function', 'options.dataFunc must be a valid function in basic scheme');
 
-    var settings = Hoek.clone(options);
+    const settings = Hoek.clone(options);
 
-    var scheme = {
-        authenticate: function (request, reply) {
-            return reply.continue({ credentials: settings.dataFunc() });
+    const scheme = {
+        authenticate: function (request, h) {
+            return h.authenticated({ credentials: settings.dataFunc() });
         }
     };
 
@@ -29,3 +22,7 @@ internals.implementation = function (server, options) {
 };
 
 
+module.exports = {
+    register,
+    pkg: require('./package.json')
+};
